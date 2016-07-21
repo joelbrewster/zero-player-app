a4beb99a91205ccb4deae0375ab0267861969e1e
Joel Brewster <j.n.brewster@gmail.com>
2016-07-21 12:54:01 +1000

    Added app init

---
+++ zero-player.app/Contents/Resources/app/js/help.js
@@ -0,0 +1,1 @@
+function help(){vex.dialog.buttons.YES.text="Start",vex.dialog.alert('Copy the shapes from below onto the grid: <img src="images/patterns.png" width="420" height="320" alt="Patterns" />')}
